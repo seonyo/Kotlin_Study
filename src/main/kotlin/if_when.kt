4c6a03e06c401e206e8ea3a785efd32025eee042
seonyo <jsy6535@naver.com>
2023-03-30 19:41:59 +0900

    when문 간단하게 사용하는 방법

--- src/main/kotlin/if_when.kt
+++ src/main/kotlin/if_when.kt
@@ -11,11 +11,18 @@ fun main(){
     }
 
     //when은 switch문 같은 것
-    var jumsu : Int = (count / 10) * 10
-    when (jumsu){
+    var jumsu1 : Int = (count / 10) * 10
+    when (jumsu1){
         100 -> println("when문: 합격(장학생)")
         90 -> println("when문: 합격(장학생)")
         80,70,60-> println("when: 합격")
         else -> println("when문: 불합격")
+    }
+
+    var jumsu2 : Int = count
+    when (jumsu2){
+        in 90..100 -> println("when문: 합격(장학생)")
+        in 60..100 -> println("when문: 합격")
+        else -> println ("when문: 불합격")
     }
 }
