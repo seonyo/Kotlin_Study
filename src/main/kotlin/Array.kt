c4282e80c8ccdb0d3ec51c605bd312679cee2009
seonyo <jsy6535@naver.com>
2023-03-30 20:02:07 +0900

    일차원 배열

---
+++ src/main/kotlin/Array.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //방이 4개인 일차형 배열을 생성 후 0으로 초기화
+    //var 배열명 = Array<데이터 형식>(개수, {초깃값})
+    var one = Array<Int> (4,{0})
+    one [0] = 10
+    one [3] = 20
+
+    println(one[0])
+    println(one[1])
+    println(one[2])
+    println(one[3])
+
+}
