e9ff4ed3dc7b5fb4d4bfa989de0ee79423a72c1a
seonyo <jsy6535@naver.com>
2023-04-04 19:35:37 +0900

    주석 수정 - 반복문을 이용한 이차원배열 출력

--- src/main/kotlin/ArrayIndex.kt
+++ src/main/kotlin/ArrayIndex.kt
@@ -3,7 +3,7 @@ fun main(){
     var one = Array<Int> (4,{0})
     one[0] = 10
     one[3] = 20
-    for(i in one.indices)
+    for(i in one.indices)  //indices는 index의 복수형. 반복문이 처음 시작되면 i는 0이 되고 그 다음은 1... 쭉 이렇게 됨
         println(one[i])
 
     var two = Array<IntArray>  (3,{IntArray(4,{2})})
@@ -13,7 +13,14 @@ fun main(){
     two[1][2]=100
     two[2][3]=200
 
-    for(i in two.indices)
-        for(j in two[i].indices)
-            println(two[i][j])
+    for((i,row) in two.withIndex())  {  //two.withIndex는 행에 대한 것. 한 행에 대한 정보를 반환받을 수 있다. i는 index 번호와 row는 객체참조값
+        for((j, col) in row.withIndex()) { //j는 열의 처음 시작 번호, col은 j열에 해당하는 값
+            print("($i. $j) = $col\t")  //$는 변수 안에 있는 값을 출력하는 것
+        }
+        println()
+    }
+
+    var three : IntArray = intArrayOf(1,2,3)  //three라는 배열에 값을 1,2,3을 차례대로 초기화시킴
+
+
 }
