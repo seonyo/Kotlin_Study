95ba4d0caad81cd2f1d5ddb708945afd67dd4d34
seonyo <jsy6535@naver.com>
2023-04-06 19:35:09 +0900

    정적필드, 정젝 메소드

--- src/main/kotlin/class.kt
+++ src/main/kotlin/class.kt
@@ -7,22 +7,26 @@ fun main(){
     var myCar3 : Car = Car("초록", 0)
 
 
-    myCar1.upSpeed(50)
-    println("자동차1의 색상은 " + myCar1.color+ "이며, 속도는 "+ myCar1.speed +"km 입니다.");
+    println("생성된 차의 대수(정적 필드) ==> " + Car.carCount)
+    println("생성된 차의 대수(정적 메소드 ==>" + Car.currentCarCount())
+    println("차의 최고 제한 속도 ==>" + Car.MAXSPEED)
 
-
-    myCar2.upSpeed(20)
-    println("자동차1의 색상은 " + myCar2.color+ "이며, 속도는 "+ myCar2.speed +"km 입니다.");
-
-    myCar3.upSpeed(250)
-    println("자동차1의 색상은 " + myCar3.color+ "이며, 속도는 "+ myCar3.speed +"km 입니다.");
-
+    println("PI의 값 ==> " + Math.PI)
+    println("3의 5제곱 ==>" + Math.pow(3.0, 5.0))
 }
 
 class Car{
     var color : String =""
     var speed : Int = 0
 
+    companion object{
+        var carCount:Int=0
+        const val MAXSPEED : Int = 200
+        const val MINSPEED : Int = 0
+        fun currentCarCount() : Int{
+            return carCount
+        }
+    }
     constructor(color: String, speed: Int) {
         this.color = color
         this.speed = speed
